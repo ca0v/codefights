07690e0fa9db1d82a960e71a617b7d583f747488
calix <corey.alix@infor.com>
2022-04-25 17:24:02 -0400

    TODO: findMedianSortedArrays in typescript

---
+++ leetcode/mediansortedarrays.ts
@@ -0,0 +1,9 @@
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+    // start from middle of 1st array
+    // how many items in 2nd array are left of that middle?
+    // how many items in 2nd array are right of that middle?
+    // if left and right are equal, then we have the median
+    // if left is greater than right, then we slide left half the difference
+    // repeat?
+    return 0;
+}
