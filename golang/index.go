577eea889f9eb40f2ed0930c72c75f1aab9b7ef0
Corey Alix <coreyalix@hotmail.com>
2022-05-07 11:24:03 -0400

    Extend rustlang and golang sample templates

--- golang/index.go
+++ golang/index.go
@@ -2,17 +2,47 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"math/rand"
 	"time"
-	"log"
 )
 
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World! %s", time.Now())
+import "math"
+
+func celciusToFahrenheit(c float64) float64 {
+	return c*9/5 + 32
 }
 
+func add(x int, y int) int {
+	return 10 * x + y
+}
+
+func swap(x, y int) (int, int) {
+	return y, x
+}
+
+// factored variable declarations
+var (
+	isIt = true
+	better bool
+	toBe   bool       = false
+	orNot  bool       = true
+)
+
 func main() {
-	log.Println("Server started on: http://localhost:8080")
-	http.HandleFunc("/", greet)
-	http.ListenAndServe(":8080", nil)
+	// explicit variable declarations
+	var a, b bool = true, false
+	var c bool
+
+	// implicit variable declarations
+	d := a != b
+	c = d
+	fmt.Println(a, b, c, isIt, better, toBe, orNot)
+	fmt.Println("Hello, world: %q", add(200, 22))
+	fmt.Println("Time:", time.Now())
+	fmt.Println("Random:", rand.Intn(100))
+	fmt.Println("Pi:", math.Pi)
+	fmt.Print("math", math.Sqrt(2))
+	fmt.Println("math", 1/math.Sin(math.Pi/4))
+	fmt.Println("Swap", add(swap(1, 2)))
+	fmt.Println("Celcius to Fahrenheit", celciusToFahrenheit(-40), celciusToFahrenheit(0), celciusToFahrenheit(100))
 }
--- rustlang/index.rs
+++ rustlang/index.rs
@@ -1,3 +1,20 @@
 fn main() {
     println!("{}", 100);    
+    println!("{foo} {bar}", foo=100, bar=200);    
+
+    // left pad with " "
+    println!("{:.>width$}", "foo", width=8);
+    println!("{:.>width$}", "foobar", width=8);
+
+    // left pad with "0"
+    println!("{:0>width$}", 9, width=5);
+
+    let x = 10;
+    let y = {
+        let a = 10;
+        let b = 20;
+        a + b + x
+    };
+
+    println!("{} {}", x, y);
 }
